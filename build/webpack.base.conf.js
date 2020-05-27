d4d4eed4b9be56ae41a4b7684725d2c6f71622b6
李委洋 <15658150268@163.com>
2020-05-27 11:26:24 +0800

    cdn改为七牛链接

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -15,6 +15,12 @@ module.exports = {
   entry: {
     app: './src/main.js'
   },
+  externals: {
+    'vue': 'Vue',
+    'axios': 'axios',
+    'vue-router': 'VueRouter',
+    'v-calendar': 'VCalendar'
+  },
   output: {
     path: config.build.assetsRoot,
     filename: '[name].js',
